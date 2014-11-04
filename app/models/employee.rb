74ed77a854cd45348c936f40aefc7e28f61fba95
MikeAllison <mikeallison79@gmail.com>
2014-11-04 14:35:32 -0500

    Added admin scope to Employees

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,6 +2,8 @@ class Employee < ActiveRecord::Base
   before_save { self.user_name = user_name.downcase }
   
   has_many :tickets
+  
+  scope :admin, -> { where(admin: true) }
 
 	def last_first
 		last_name + ', ' + first_name
--- db/migrate/20141104140841_add_admin_to_employees.rb
+++ db/migrate/20141104140841_add_admin_to_employees.rb
@@ -1,6 +1,6 @@
 class AddAdminToEmployees < ActiveRecord::Migration
   def change
-    add_column :employees, :admin, :boolean
+    add_column :employees, :admin, :boolean, default: false
     add_index :employees, :admin
   end
 end
