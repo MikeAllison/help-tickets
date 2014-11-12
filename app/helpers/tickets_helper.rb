9875256441399f0bc780281f76fc39ce93a0f9c7
MikeAllison <mikeallison79@gmail.com>
2014-11-12 14:40:49 -0500

    Moved ticket view category function into tickets_helper

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,2 +1,15 @@
 module TicketsHelper
+  
+  def action_name
+    status = params[:action].split('_')
+    status.map { |item| item.capitalize! }
+    status = status.join(' ')
+    
+    if status == 'Index'
+      status = 'All'
+    else
+      status
+    end
+  end
+  
 end
