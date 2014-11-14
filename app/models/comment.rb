52c8f2daaa410a3d91264ed36b6cac86875830cb
MikeAllison <mikeallison79@gmail.com>
2014-11-14 16:42:47 -0500

    Created before_save action to set employee_id for comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,15 @@
 class Comment < ActiveRecord::Base
+  before_save :set_employee_id
   
   belongs_to :ticket
   belongs_to :employee
   
   validates :body, presence: true
-
+  
+  private
+  
+  def set_employee_id
+    # Trying to set to the id of the logged in employee from SessionsHelper
+    self.employee_id = 1
+  end
 end
