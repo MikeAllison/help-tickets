18e99178490ff5e53167dca048d9c316ad875ca8
MikeAllison <mikeallison79@gmail.com>
2014-11-18 16:19:11 -0500

    Moved panel header function to app helper.  Created panel partial for all controllers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
-module ApplicationHelper  
+module ApplicationHelper
+  
+  def panel_header
+    action_name = params[:action].split('_')
+    action_name.map { |item| item.capitalize! }
+    action_name = action_name.join(' ')
+    
+    controller_name = params[:controller].split('_')
+    controller_name.map { |item| item.capitalize! }
+    controller_name = controller_name.join(' ')
+    
+    if action_name == 'Index'
+      action_name = 'All'
+    end
+    # Need to add count to end of header
+    action_name + ' ' + controller_name
+  end
+  
 end
--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,17 +1,5 @@
 module TicketsHelper
-  
-  def action_name
-    status = params[:action].split('_')
-    status.map { |item| item.capitalize! }
-    status = status.join(' ')
-    
-    if status == 'Index'
-      status = 'All'
-    else
-      status
-    end
-  end
-  
+   
   # Creates a link for table headers with params to sort 
   def sort_column(title, column, join_table = nil)
     if column == params[:sort_by] && params[:direction] == "ASC"
