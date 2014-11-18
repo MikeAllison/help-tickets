8c9167c83f799e48b6857a4360ef8ad374892114
MikeAllison <mikeallison79@gmail.com>
2014-11-18 16:46:19 -0500

    Factored out panel with form partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,15 +2,18 @@ module ApplicationHelper
   
   def panel_header
     action_name = params[:action].split('_')
-    action_name.map { |item| item.capitalize! }
-    action_name = action_name.join(' ')
-    
     controller_name = params[:controller].split('_')
+    
+    action_name.map { |item| item.capitalize! }
     controller_name.map { |item| item.capitalize! }
+    
+    action_name = action_name.join(' ')
     controller_name = controller_name.join(' ')
     
     if action_name == 'Index'
       action_name = 'All'
+    elsif action_name == 'New'
+      controller_name.chop!
     end
     # Need to add count to end of header
     action_name + ' ' + controller_name
