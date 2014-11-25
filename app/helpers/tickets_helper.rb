ba5c129e956e36954d859454c82efb44c987ed8d
MikeAllison <mikeallison79@gmail.com>
2014-11-25 12:51:31 -0500

    Created status_icon helper for tickets views

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,2 +1,18 @@
-module TicketsHelper    
+module TicketsHelper
+  
+  # Sets color of flag icon in tickets/list partial
+  def status_icon(state)
+    if state == 'Unassigned'
+      style = 'danger'
+    elsif state == 'Work in Progress'
+      style = 'warning'
+    elsif state == 'On Hold'
+      style = 'warning'
+    elsif state == 'Closed'
+      style = 'success'
+    end
+    
+    content_tag :span, '', class: 'glyphicon glyphicon-flag alert-' + style
+  end
+   
 end
