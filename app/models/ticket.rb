5ffe4ddb3ef8ec22582e43bb0e7ba635b04fedc6
Mike Allison <mikeallison79@gmail.com>
2014-12-16 19:16:38 -0500

    Added dependent destroy on ticket comments and attachments

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,8 +2,8 @@ class Ticket < ActiveRecord::Base
 	
 	before_save :set_status
 
-  has_many :attachments
-  has_many :comments
+  has_many :attachments, dependent: :destroy
+  has_many :comments, dependent: :destroy
   belongs_to :creator, class_name: 'Employee'
   belongs_to :technician, class_name: 'Employee'
   belongs_to :topic
