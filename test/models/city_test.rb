b50bf6b63b3b94c87a3270946e67e9dadc40af7a
Mike Allison <mikeallison79@gmail.com>
2015-09-09 08:35:58 -0400

    Added more unit tests for City

--- test/models/city_test.rb
+++ test/models/city_test.rb
@@ -4,18 +4,11 @@ class CityTest < ActiveSupport::TestCase
 
   def setup
     @c = cities(:orlando)
+    @c2 = cities(:sanfran)
   end
 
   test 'should not save without a name' do
     assert_not_blank(@c, :name)
-  end
-
-  test 'should strip whitespace in name before save' do
-    should_strip_whitespace(@c, :name)
-  end
-
-  test 'should not save without a state_id' do
-    assert_not_blank(@c, :state_id)
   end
 
   test 'should not allow save of duplicate city/states' do
@@ -28,8 +21,27 @@ class CityTest < ActiveSupport::TestCase
     assert city2.save
   end
 
+  test 'should not save without a state_id' do
+    assert_not_blank(@c, :state_id)
+  end
+
+  test 'should strip whitespace in name before save' do
+    should_strip_whitespace(@c, :name)
+  end
+
   test 'city_state_abbr' do
     assert_equal 'Orlando, FL', @c.city_state_abbr
+  end
+
+  test 'unhide' do
+    city2 = City.create(name: 'Tampa', state_id: states(:florida).id, hidden: true)
+    city2.unhide
+    assert_not city2.hidden
+  end
+
+  test 'create_slug' do
+    @c2.save
+    assert_equal 'san-francisco-ca', @c2.slug
   end
 
 end
