bc75c7ac823c7fbe6f353204c699358c7911e5b4
Mike Allison <mikeallison79@gmail.com>
2015-10-27 21:55:25 -0400

    Started writing tests for SessionsController.

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,9 +2,22 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
 
+  def setup
+    @active_nontech = employees(:inactive_nontech)
+    @active_tech = employees(:inactive_tech)
+  end
+
   test 'login page is accessible when not logged in' do
     get :new
     assert_response :success
+  end
+
+  test 'inactive non-techs cannot log in' do
+    log_in(@)
+  end
+
+  test 'inactive techs cannot log in' do
+
   end
 
 end
