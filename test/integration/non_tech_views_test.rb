f011190a3ae660c69b14a6230085ea341b2ce65c
Mike Allison <mikeallison79@gmail.com>
2015-11-17 16:26:12 -0500

    Added some view tests.  Fixed minor issue in tickets_list.

---
+++ test/integration/non_tech_views_test.rb
@@ -0,0 +1,40 @@
+require 'test_helper'
+
+class NonTechViewsTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @active_nontech = employees(:active_nontech)
+    integration_login(@active_nontech)
+  end
+
+  def teardown
+    logout!
+  end
+
+  test '/employees/:id/edit DOES NOT show field: Account Status' do
+    visit "/employees/#{@active_nontech.username}/edit"
+    assert page.has_no_css?('label', text: /Account Status/)
+    assert page.has_no_field?('employee_active_0', type: 'radio')
+    assert page.has_no_field?('employee_active_1', type: 'radio')
+  end
+
+  test '/employees/:id/edit DOES NOT show field: User Rights' do
+    visit "/employees/#{@active_nontech.username}/edit"
+    assert page.has_no_css?('label', text: /User Rights/)
+    assert page.has_no_field?('employee_technician_0', type: 'radio')
+    assert page.has_no_field?('employee_technician_1', type: 'radio')
+  end
+
+  test '/tickets/new DOES NOT show field: Select Status' do
+    visit "/tickets/new"
+    assert page.has_no_css?('label', text: /Select Status/)
+    assert page.has_no_select?('ticket_status')
+  end
+
+  test '/tickets/new DOES NOT show field: Assigned To' do
+    visit "/tickets/new"
+    assert page.has_no_css?('label', text: /Assigned To/)
+    assert page.has_no_select?('ticket_technician_id')
+  end
+
+end
--- test/integration/technician_views_test.rb
+++ test/integration/technician_views_test.rb
@@ -4,6 +4,7 @@ class TechnicianViewsTest < ActionDispatch::IntegrationTest
 
   def setup
     @active_tech = employees(:active_tech)
+    @active_nontech = employees(:active_nontech)
     integration_login(@active_tech)
   end
 
@@ -11,28 +12,55 @@ class TechnicianViewsTest < ActionDispatch::IntegrationTest
     logout!
   end
 
-  test 'tickets assigned field shows on employees/technicians' do
-    click_link 'Employees'
-    click_link 'Technician Employees'
-    assert page.has_css?('table', text: /Tickets Assigned/)
+  test 'employees/technicians shows field: Assigned To' do
+    visit '/employees/technicians'
+    assert page.has_css?('th', text: /Tickets Assigned/)
   end
 
-  test 'tickets assigned field does not show on employees/all' do
-    click_link 'Employees'
-    click_link 'All Employees'
-    assert page.has_no_css?('table', text: /Tickets Assigned/)
+  test 'employees/all does not show field: Assigned To' do
+    visit '/employees/all'
+    assert page.has_no_css?('th', text: /Tickets Assigned/)
   end
 
-  test 'tickets assigned field does not show on employees/active' do
-    click_link 'Employees'
-    click_link 'Active Employees'
-    assert page.has_no_css?('table', text: /Tickets Assigned/)
+  test 'employees/active does not show field: Assigned To' do
+    visit '/employees/active'
+    assert page.has_no_css?('th', text: /Tickets Assigned/)
   end
 
-  test 'tickets assigned field does not show on employees/inactive' do
-    click_link 'Employees'
-    click_link 'Inactive Employees'
-    assert page.has_no_css?('table', text: /Tickets Assigned/)
+  test 'employees/inactive does not show field: Assigned To' do
+    visit '/employees/inactive'
+    assert page.has_no_css?('th', text: /Tickets Assigned/)
+  end
+
+  test 'employees/:id/assigned_tickets does not show field: Assigned To' do
+    visit "/employees/#{@active_tech.username}/assigned_tickets"
+    assert page.has_no_css?('th', text: /Assigned To/)
+  end
+
+  test '/employees/:id/edit shows field: Account Status' do
+    visit "/employees/#{@active_nontech.username}/edit"
+    assert page.has_css?('label', text: /Account Status/)
+    assert page.has_field?('employee_active_0', type: 'radio')
+    assert page.has_field?('employee_active_1', type: 'radio')
+  end
+
+  test '/employees/:id/edit shows field: User Rights' do
+    visit "/employees/#{@active_nontech.username}/edit"
+    assert page.has_css?('label', text: /User Rights/)
+    assert page.has_field?('employee_technician_0', type: 'radio')
+    assert page.has_field?('employee_technician_1', type: 'radio')
+  end
+
+  test '/tickets/new shows field: Select Status' do
+    visit "/tickets/new"
+    assert page.has_css?('label', text: /Select Status/)
+    assert page.has_select?('ticket_status')
+  end
+
+  test '/tickets/new shows field: Assigned To' do
+    visit "/tickets/new"
+    assert page.has_css?('label', text: /Assigned To/)
+    assert page.has_select?('ticket_technician_id')
   end
 
 end
